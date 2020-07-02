97cf14bfd652b1e5002898b9aa22d9739c3a600a
Joseph Farruggio <joey@josephallendesign.com>
2020-07-02 11:08:02 -0500

    Messenger WIP

--- chatbot-optimizer.php
+++ chatbot-optimizer.php
@@ -122,7 +122,8 @@ if ( get_option('_chat_provider') === 'intercom' ) {
 // Messenger selected
 if ( get_option('_chat_provider') === 'messenger' ) {
   if ( get_option('_messenger_id') != null ) {
-    function optimize_messenger_enqueue_script() {   	
+    function optimize_messenger_enqueue_script() {  
+      wp_enqueue_style( 'messenger-button', plugin_dir_url( __FILE__ ) . 'dist/css/messenger-button.min.css'); 	
       wp_enqueue_script( 'optimized_messenger', plugin_dir_url( __FILE__ ) . 'dist/js/messenger-init.min.js', array(), null, true);
     }
     add_action('wp_enqueue_scripts', 'optimize_messenger_enqueue_script');
@@ -130,7 +131,7 @@ if ( get_option('_chat_provider') === 'messenger' ) {
     function insert_my_footer() {
       echo "<div id='fb-root'></div>";
       echo "<div class='fb-customerchat' attribution=setup_tool page_id='". get_option('_messenger_id') ."'></div>";
-      // echo '<button onmouseenter="LoadChatWidget()" id="intercom-button" class="intercom-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 32"><path d="M28 32s-4.714-1.855-8.527-3.34H3.437C1.54 28.66 0 27.026 0 25.013V3.644C0 1.633 1.54 0 3.437 0h21.125c1.898 0 3.437 1.632 3.437 3.645v18.404H28V32zm-4.139-11.982a.88.88 0 00-1.292-.105c-.03.026-3.015 2.681-8.57 2.681-5.486 0-8.517-2.636-8.571-2.684a.88.88 0 00-1.29.107 1.01 1.01 0 00-.219.708.992.992 0 00.318.664c.142.128 3.537 3.15 9.762 3.15 6.226 0 9.621-3.022 9.763-3.15a.992.992 0 00.317-.664 1.01 1.01 0 00-.218-.707z"></path></svg></button>';
+      echo '<button onmouseenter="LoadChatWidget()" onClick="OpenChatWidget()" id="messenger-button" class="messenger-button"><svg width="60px" height="60px" viewBox="0 0 60 60"><svg x="0" y="0" width="60px" height="60px"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g><circle fill="#0084FF" cx="30" cy="30" r="30"></circle><svg x="10" y="10"><g transform="translate(0.000000, -10.000000)" fill="#FFFFFF"><g id="logo" transform="translate(0.000000, 10.000000)"><path d="M20,0 C31.2666,0 40,8.2528 40,19.4 C40,30.5472 31.2666,38.8 20,38.8 C17.9763,38.8 16.0348,38.5327 14.2106,38.0311 C13.856,37.9335 13.4789,37.9612 13.1424,38.1098 L9.1727,39.8621 C8.1343,40.3205 6.9621,39.5819 6.9273,38.4474 L6.8184,34.8894 C6.805,34.4513 6.6078,34.0414 6.2811,33.7492 C2.3896,30.2691 0,25.2307 0,19.4 C0,8.2528 8.7334,0 20,0 Z M7.99009,25.07344 C7.42629,25.96794 8.52579,26.97594 9.36809,26.33674 L15.67879,21.54734 C16.10569,21.22334 16.69559,21.22164 17.12429,21.54314 L21.79709,25.04774 C23.19919,26.09944 25.20039,25.73014 26.13499,24.24744 L32.00999,14.92654 C32.57369,14.03204 31.47419,13.02404 30.63189,13.66324 L24.32119,18.45264 C23.89429,18.77664 23.30439,18.77834 22.87569,18.45674 L18.20299,14.95224 C16.80079,13.90064 14.79959,14.26984 13.86509,15.75264 L7.99009,25.07344 Z"></path></g></g></svg></g></g></svg></svg></button>';
     }
     add_action('wp_footer', 'insert_my_footer');
   }
--- dist/js/messenger-init.js
+++ dist/js/messenger-init.js
@@ -13,11 +13,29 @@ window.fbAsyncInit = function() {
   });
 };
 
-(function(d, s, id) {
+var LoadChatWidget = function(){ (function(d, s, id) {
 var js, fjs = d.getElementsByTagName(s)[0];
 if (d.getElementById(id)) return;
 js = d.createElement(s); js.id = id;
 js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+};
 
+function OpenChatWidget() {
+  FB.Event.subscribe('customerchat.load', FB.CustomerChat.showDialog());
+}
+
+
+var chatIsLoaded = false;
+
+if (chatIsLoaded === false) {
+  // load after waiting 5000 milliseconds, 5 seconds
+  setTimeout((function(){ 
+    LoadChatWidget();
+    chatIsLoaded = true;
+    var button = document.getElementById('messenger-button');
+    button.parentNode.removeChild(button);
+    FB.CustomerChat.showDialog();
+  }), 5000);
+}
